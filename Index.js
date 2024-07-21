791461483e90d0c5b47171c59b06516404bf2c1d
Ajay1455 <75538701+Ajay1455@users.noreply.github.com>
2024-07-21 16:31:40 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3000;
 
+app.use(cors())
 const dishes = [
   {
     img: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Pizzas.png",
