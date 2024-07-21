75b5a896f4abf876c399a27667288a814a97c182
Ajay1455 <75538701+Ajay1455@users.noreply.github.com>
2024-07-21 16:29:59 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -66,75 +66,107 @@ const dishes = [
 ];
 
 const restaurants = [
-  {
-    image:
-      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/1/4a8bc67d-8fe4-44db-be5d-1477086cc52b_65268.JPG",
-    offer: "₹70 OFF ABOVE ₹249",
-    title: "McDonald's",
-    rating: 4.4,
-    minTime: 30,
-    maxTime: 35,
-    name: "American",
-    place: "Rawaton Ka Bass",
-  },
-  {
-    image:
-      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/9/ede9fe2b-8236-48a0-81d6-f3cbb9f07e37_580591.jpg",
-    offer: "₹70 OFF ABOVE ₹249",
-    title: "GetAWay-Ice Creams & Desserts",
-    rating: 4.4,
-    minTime: 30,
-    maxTime: 35,
-    name: "Ice Cream, Desserts, Healthy Food, Sweets",
-    place: "City Center",
-  },
-  {
-    image:
-      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/6/20/76b4a16b-9c1a-4b51-84bb-6ae7e5b54d4e_94492.JPG",
-    offer: "₹70 OFF ABOVE ₹249",
-    title: "Keventers - Milkshakes & Desserts",
-    rating: 4.4,
-    minTime: 30,
-    maxTime: 35,
-    name: "Beverages, Ice Cream, Desserts, Healthy Food",
-    place: "Birla Nagar",
-  },
-  {
-    image:
-      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/6/13/f0f4f88a-4952-4a93-82e8-f8dce80e10f8_408363.JPG",
-    offer: "₹70 OFF ABOVE ₹249",
-    title: "Kwality Walls Frozen Dessert and Ice Cream Shop",
-    rating: 4.4,
-    minTime: 30,
-    maxTime: 35,
-    name: "Desserts, Ice Cream, Ice Cream Cakes",
-    place: "City Center",
-  },
-  {
-    image:
-      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/1/4a8bc67d-8fe4-44db-be5d-1477086cc52b_65268.JPG",
-    offer: "₹70 OFF ABOVE ₹249",
-    title: "McDonald's",
-    rating: 4.4,
-    minTime: 30,
-    maxTime: 35,
-    name: "American",
-    place: "Rawaton Ka Bass",
-  },
-  {
-    image:
-      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/1/4a8bc67d-8fe4-44db-be5d-1477086cc52b_65268.JPG",
-    offer: "₹70 OFF ABOVE ₹249",
-    title: "Subway",
-    rating: 4.4,
-    minTime: 30,
-    maxTime: 35,
-    name: "Healthy Food, Salads, Snacks, Desserts, Beverages",
-    place: "City Center",
-  },
-
-];
-
+    {
+        "image": "2b4f62d606d1b2bfba9ba9e5386fabb7.jpeg",
+        "offer": "Items at ₹179",
+        "title": "Pizza Hut",
+        "rating": 4.2,
+        "minTime": 30,
+        "maxTime": 40,
+        "name": "Pizzas",
+        "place": "New Jodhpur"
+    },
+    {
+        "image": "75d0b3ebeb56fe2484c944e94cac7a8d.jpeg",
+        "offer": "₹50 OFF ABOVE ₹199",
+        "title": "Janta Sweet Home",
+        "rating": 4.5,
+        "minTime": 35,
+        "maxTime": 40,
+        "name": "Sweets,South Indian",
+        "place": "Shastri Nagar"
+    },
+    {
+        "image": "cef5bf4cc31dc3c46a1e1b027c5627e4.jpeg",
+        "offer": "₹85 OFF ABOVE ₹149",
+        "title": "Parihaar Bhojnalay",
+        "rating": 4.3,
+        "minTime": 35,
+        "maxTime": 40,
+        "name": "North India, Thalis",
+        "place": "Sardarpura"
+    },
+    {
+        "image": "eillc7d7hbrzcuussus5.jpeg",
+        "offer": "₹70 OFF ABOVE ₹149",
+        "title": "Kwality Walls Frozen",
+        "rating": 4.5,
+        "minTime": 25,
+        "maxTime": 25,
+        "name": "Desserts,Ice Cream",
+        "place": "Chopsani Housing Board"
+    },
+    {
+        "image": "6e04be27387483a7c00444f8e8241108.jpeg",
+        "offer": "₹1-5 OFF ABOVE ₹179",
+        "title": "The Good Bowl",
+        "rating": 4.4,
+        "minTime": 30,
+        "maxTime": 35,
+        "name": "Biryani, North India",
+        "place": "Shastri Nagar"
+    },
+    {
+        "image": "w7ndo1dkkk6kf4qfz2p1.jpeg",
+        "offer": "₹100 OFF ABOVE ₹499",
+        "title": "NIC Ice Creams",
+        "rating": 4.7,
+        "minTime": 25,
+        "maxTime": 30,
+        "name": "Desserts,Ice Cream",
+        "place": "Sardarpura"
+    },
+    {
+        "image": "f1bc9ddf53de574cdc35ab2f717df234.jpeg",
+        "offer": "Items at ₹109",
+        "title": "Dosh Center",
+        "rating": 4.0,
+        "minTime": 35,
+        "maxTime": 40,
+        "name": "South Indian",
+        "place": "Chopsani Housing Board"
+    },
+    {
+        "image": "919cb3be0e7406f86f6741ebe7c30128.jpeg",
+        "offer": "₹125 OFF ABOVE ₹349",
+        "title": "Lunch Box - Meals and Thalis",
+        "rating": 4.3,
+        "minTime": 30,
+        "maxTime": 35,
+        "name": "Biryani, North India",
+        "place": "Shastri Nagar"
+    },
+    {
+        "image": "535fc9f9c135f7982317bbb6a64a1ffc.jpeg",
+        "offer": "₹70 OFF ABOVE ₹249",
+        "title": "McDonald's",
+        "rating": 4.4,
+        "minTime": 30,
+        "maxTime": 35,
+        "name": "American",
+        "place": "Rawaton Ka Bass"
+    },
+    {
+        "image": "2a41aa363a8ae1e98a4cce95c2d0c589.jpeg",
+        "offer": "₹100 OFF ABOVE ₹449",
+        "title": "Kajal's Cake",
+        "rating": 4.5,
+        "minTime": 35,
+        "maxTime": 40,
+        "name": "Cake",
+        "place": "Chopsani Housing Board"
+    }
+]
 
 app.use(express.json());
 
